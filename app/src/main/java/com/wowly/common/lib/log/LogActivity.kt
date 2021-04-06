fd562e1bcb46edac2ff5cf4c713a3742cc858673
lion <zqhusixia@163.com>
2021-04-06 15:26:04 +0800

    add lib_logger

---
+++ app/src/main/java/com/wowly/common/lib/log/LogActivity.kt
@@ -0,0 +1,13 @@
+package com.wowly.common.lib.log
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wowly.common.lib.log.R.*
+
+
+class LogActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layout.activity_main)
+    }
+}
